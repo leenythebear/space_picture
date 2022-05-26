1a904029c28085cbdb531ab4a7397a61cfb791ff
leenythebear <Leeny@inbox.ru>
2022-05-26 17:07:22 +0700

    make func for save pictures

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ import os
 if not os.path.exists("images"):
     os.makedirs("images")
 
-filename = 'hubble.jpeg'
-url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
-response = requests.get(url)
-response.raise_for_status()
+def save_picture(url, path, filename='hubble.jpeg'):
+    response = requests.get(url)
+    response.raise_for_status()
+    with open(f'images/{filename}', 'wb') as file:
+        file.write(response.content)
 
-with open(f'images/{filename}', 'wb') as file:
-    file.write(response.content)
 
+# save_picture('https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg', "/images")
