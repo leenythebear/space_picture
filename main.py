236db96710ca4b0282f9fcd24a25ad507be4d1bf
leenythebear <Leeny@inbox.ru>
2022-05-26 16:32:07 +0700

    make a dir for save the pic

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import requests
+import os
 
+if not os.path.exists("images"):
+    os.makedirs("images")
 
 filename = 'hubble.jpeg'
 url = 'https://upload.wikimedia.org/wikipedia/commons/3/3f/HST-SM4.jpeg'
 response = requests.get(url)
 response.raise_for_status()
 
-with open(filename, 'wb') as file:
+with open(f'images/{filename}', 'wb') as file:
     file.write(response.content)
 
