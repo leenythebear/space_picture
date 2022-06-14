9b8b998bd117cd2d26c91546708850ccf4fb6a06
leenythebear <Leeny@inbox.ru>
2022-06-14 21:35:26 +0700

    make helper script

---
+++ helper.py
@@ -0,0 +1,22 @@
+import requests
+import os
+from urllib.parse import urlparse
+
+
+def create_folder(folder_name):
+    if not os.path.exists(folder_name):
+        os.makedirs(folder_name)
+
+
+def save_picture(index, url, folder_name, filename, extension, token=None):
+    params = {"api_key": f"{token}"}
+    response = requests.get(url, params=params)
+    response.raise_for_status()
+    with open(f'{folder_name}/{filename}{index}{extension}', 'wb') as file:
+        file.write(response.content)
+
+
+def get_picture_extension(url):
+    parsed_link = urlparse(url)
+    parsed_path = os.path.splitext(parsed_link.path)
+    return parsed_path[1]
