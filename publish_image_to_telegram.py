06e24ee0e2c76b7605bf7cb593b8317dfd20907d
leenythebear <Leeny@inbox.ru>
2022-06-14 21:34:07 +0700

    make func for publish image to telegram

---
+++ publish_image_to_telegram.py
@@ -0,0 +1,37 @@
+from dotenv import load_dotenv
+import telegram
+import os
+import random
+
+load_dotenv()
+
+CHAT_ID = "@cosmicboybot2022"
+BOT_TOKEN = os.environ['BOT_TOKEN']
+IMAGES_FOLDER = 'images'
+
+
+def publish_image(images_paths):
+    bot = telegram.Bot(token=BOT_TOKEN)
+    image_path = random.choice(paths)
+    bot.send_photo(chat_id=CHAT_ID, photo=open(image_path, 'rb'))
+    return image_path
+
+
+def take_files():
+    files = list(os.walk(IMAGES_FOLDER))
+    names = files[0][-1]
+    images_paths = []
+    for name in names:
+        images_paths.append(files[0][0] + "/" + name)
+    return images_paths
+
+
+def del_image(image_path):
+    os.remove(image_path)
+    print(image_path)
+
+
+if __name__ == '__main__':
+    paths = take_files()
+    path = publish_image(paths)
+    del_image(path)
