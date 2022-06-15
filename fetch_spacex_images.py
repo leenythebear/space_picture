e913346e2759e41a3b1e3a608a113810acd12eac
leenythebear <Leeny@inbox.ru>
2022-06-15 12:34:25 +0700

    rename global variables

--- fetch_spacex_images.py
+++ fetch_spacex_images.py
@@ -20,7 +20,7 @@ def fetch_spacex_images(links):
     create_folder(IMAGES_FOLDER)
     for index, link in enumerate(links):
         extension = get_picture_extension(link)
-        save_picture(index, link, IMAGES_FOLDER, FILE_NAME, extension)
+        save_picture(index, link, IMAGES_FOLDER, FILE_NAME_SPACEX, extension)
 
 
 if __name__ == '__main__':
@@ -28,6 +28,6 @@ if __name__ == '__main__':
     parser.add_argument('id', help='ID полета для выгрузки фотографий')
     args = parser.parse_args()
     flight_id = urlparse(args.id)
-    url = BASE_URL + f'/{flight_id.path}'
+    url = SPACEX_URL + f'/{flight_id.path}'
     spacex_images_links = get_spacex_images_links(url)
     fetch_spacex_images(spacex_images_links)
